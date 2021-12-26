d84037b9042cfbe0eb9f8f1b4987b7c4f2d653e0
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-12-26 22:21:30 +0530

    added user profile

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { client, urlFor } from "../client";
 import { MdDownloadForOffline } from "react-icons/md";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { v4 as uuid4 } from "uuid";
 
 const Pin = ({ pin }) => {
   const { postedBy, image, _id, destination } = pin;
@@ -21,8 +24,39 @@ const Pin = ({ pin }) => {
     });
   };
 
+  let alreadySaved = pin?.save?.filter(
+    (item) => item?.postedBy?._id === user?.googleId
+  );
+
+  alreadySaved = alreadySaved?.length > 0 ? alreadySaved : [];
+
+  const savePin = (id) => {
+    if (alreadySaved?.length === 0) {
+      setSavingPost(true);
+
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuid4(),
+            userId: user?.googleId,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user?.googleId,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          window.location.reload();
+          setSavingPost(false);
+        });
+    }
+  };
+
   return (
-    <div className="m-2">
+    <div className="m-2 bg-white pb-2 rounded-lg shadow-sm hover:shadow-lg transition-all duration-300">
       <div
         onMouseEnter={() => setPostHovered(true)}
         onMouseLeave={() => setPostHovered(false)}
@@ -36,7 +70,7 @@ const Pin = ({ pin }) => {
 
         {postHovered && (
           <div
-            className="absolute top-0 w-full flex flex-col justify-between p-1 pr-2 pb-2 z-50"
+            className="absolute top-0 w-full flex flex-col justify-between p-1 pr-2 pb-2 z-50 transition-all duration-600"
             style={{ height: "100%" }}
           >
             <div className="flex items-center justify-between">
@@ -47,15 +81,70 @@ const Pin = ({ pin }) => {
                   onClick={(e) => {
                     e.stopPropagation();
                   }}
-                  className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                  className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none "
                 >
                   <MdDownloadForOffline />
                 </a>
               </div>
+
+              {alreadySaved?.length !== 0 ? (
+                <button type="button" className="saveBtnStyle">
+                  {pin?.save?.length} Saved
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                  className="saveBtnStyle"
+                >
+                  {pin?.save?.length} {savingPost ? "Saving" : "Save"}
+                </button>
+              )}
+            </div>
+
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination?.slice(8).length > 0 ? (
+                <a
+                  href={destination}
+                  target="_blank"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opaci hover:opacity-100 hover:shadow-md"
+                  rel="noreferrer"
+                >
+                  <BsFillArrowUpRightCircleFill />
+                  {destination?.slice(8, 17)}...
+                </a>
+              ) : undefined}
+
+              {postedBy?._id === user?.googleId && (
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deletePin(_id);
+                  }}
+                  className="bg-white p-2 rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-75 hover:opacity-100 outline-none"
+                >
+                  <AiTwotoneDelete />
+                </button>
+              )}
             </div>
           </div>
         )}
       </div>
+      <Link
+        to={`/user-profile/${postedBy?._id}`}
+        className="flex gap-2 mt-2 justify-center items-center"
+      >
+        <img
+          src={postedBy?.image}
+          alt="user-profile"
+          className="w-8 h-8 rounded-full"
+        />
+        <p className="font-semibold capitalize">{postedBy?.userName}</p>
+      </Link>
     </div>
   );
 };
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -120,3 +120,55 @@ export const searchQuery = (searchItem) => {
 
   return query;
 };
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset -> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      }
+    }
+  }`;
+
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
