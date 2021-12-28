3a335a2b8b5dfcbea205f651ca053fb61eadf121
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-12-29 01:13:19 +0530

    finished frontend

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Spinner from "./Spinner";
+import MasonryLayout from "./MasonryLayout";
+import { feedQuery, searchQuery } from "../utils/data";
+import { client } from "../client";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ searchItem }) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchItem !== "") {
+      setLoading(true);
+
+      const query = searchQuery(searchItem.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchItem]);
+
+  return (
+    <div>
+      {loading && <Spinner />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length !== 0 && searchItem !== "" && !loading && (
+        <div className="mt-10 text-center text-xl text-gray-400">
+          <p>No Pins are Found!</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
